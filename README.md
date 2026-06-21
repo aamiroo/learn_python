843519d8fdb3662610e3d113405bfbb62d0137da
aamiroo <a.a.drv73358@gmail.com>
2026-06-21 13:37:45 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# python_learning
+Learning Python as programming language
+
+
+
+## License
+
+You can use it under [GPL](gpl-3.0.txt) license
+
---
+++ codes/variable.py
@@ -0,0 +1,19 @@
+"""
+variable in python
+"""
+
+# Variable store in RAM
+### variable:
+# * name: left hand side
+# * value: right hand side
+# * type: does not matter
+# - number
+# - string
+# name = value
+
+esm1 = "ahmad"
+sen = 20
+
+print("name shoma: ", esm1)
+print("sene shoma: ", sen)
+print("salam emrooz jomee ast! rooz bekheir")
