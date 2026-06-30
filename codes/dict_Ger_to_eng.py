7b6f9eb1f138f3bf6d0d5ae53cf3fe5f387fcdac
aamiroo <a.a.drv73358@gmail.com>
2026-06-30 13:45:56 +0330

    Increasing word data in the dictionary

---
+++ codes/dict_Ger_to_eng.py
@@ -0,0 +1,43 @@
+import json
+
+words = {}
+#import json data for dictinary
+with open("dictionary-de.json","r") as fd:
+    dictionary = {}
+    for line in fd:
+        data = json.loads(line)
+
+        german = data.get("") #giv valiu from key
+        meaning = data.get("d", []) #add to meaning
+
+        if german and meaning: #creat dictionary
+            dictionary[german.lower()] = meaning
+
+
+words['hallo'] = ['hello']
+words['buch'] = ['book']
+words['uhr'] = ['clock']
+words['welt'] = ['world']
+words['müde'] = ['tired']
+words['lrieg'] = ['war']
+words['Geschichte'] = ['history']
+words['fehler'] = ['error']
+words['kammentar'] = ['coment']
+words['König'] = ['king']
+words.update(dictionary) #add json to my dict
+
+def search (words , word):
+    if word in words:
+        return(words[word])
+    else:
+        return "not information"
+while True:
+    try: 
+        print()
+        word = input("please enter your word: \n-press enter Q for exit\n").lower()
+        if word == "q":
+            break
+        print(search (words , word))
+    except Exception as e:
+        print(e)
+
