a000d81acb4963d4cb598566ffb546ced7e41f81
aamiroo <a.a.drv73358@gmail.com>
2026-08-08 14:09:40 +0330

    Changes have been implemented, including the installation of pytest for project testing. The project tests will also be released soon.

--- codes/chat/client.py
+++ codes/chat/client.py
@@ -4,30 +4,38 @@ this is client
 """
 from colorama import init, Fore #for show color in terminal
 
-init(autoreset=True)
-import socket
-import threading
+init(autoreset=True) # Returning to the normal color state
+import socket  # for connection
+import threading  # For simultaneous chat
 
-HOST = "192.168.4.128"
-PORT = 5000
+HOST = "192.168.4.128"  # ip server
+PORT = 5000  
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+# function connection
+def connect_to_server(host,port):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # creat socket with IP v4 & tcp mode
+    client.connect((host,port)) # Socket connection to destination
+    return client
 
+client = connect_to_server(HOST, PORT) # Create connection
+
+# To receive the message
 def receive():
     while True:
         try:
-            data = client.recv(1024)
+            data = client.recv(1024)  # To receive the data from socket & 1024 Maximum number of bytes
             if not data:
                 break
-            print(f"\n{Fore.BLUE}server: {data.decode()}")
+            print(f"\n{Fore.BLUE}server: {data.decode()}")   # Converting Byte to String
         except:
             break
-threading.Thread(target=receive, daemon=True).start()
+
+# Create a separate thread and execute the `receive` function within it.
+threading.Thread(target=receive, daemon=True).start()   
 
 while True:
     print()
-    msg = input((Fore.GREEN + "you: "))
+    msg = input((Fore.GREEN + "you: ")) # for writing message
     print()
 
     if not msg:
--- codes/chat/server.py
+++ codes/chat/server.py
@@ -4,42 +4,50 @@ this is server
 """
 from colorama import init, Fore  # for show color in terminal
 
-init(autoreset=True)
+init(autoreset=True)  # Returning to the normal color state
 import socket  # for socket
-import threading
+import threading  # For simultaneous chat
 
 HOST = "0.0.0.0"  # IP for all Interface
 PORT = 5000
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IP and TCP
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create socket & IP and TCP
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # To free up the port
+
 server.bind((HOST, PORT))  # for conection Socket to IP
-server.listen(1)
+
+server.listen(1)  #For the number of connections that can be placed in the connection queue
 print(Fore.GREEN + "Waiting for connection...")
 client, address = server.accept()
 
-print("Connected:", address)
+print("Connected:", address)  # Show who is connected
+
+
+# To receive the message
 def receive():
     while True:
         try:
-            data = client.recv(1024)
+            data = client.recv(1024)  ## To receive the data from socket & 1024 Maximum number of bytes
             if not data:
                 break
             print(f"\n{Fore.BLUE}server: {data.decode()}")
         except:
             break
+
+# Create a separate thread and execute the `receive` function within it.
 threading.Thread(target=receive, daemon=True).start()
 
 try:
     while True:
         print()
-        reply = input(Fore.GREEN + "you: ")
+        reply = input(Fore.GREEN + "you: ")  # for writing message
         print()
 
         if not reply:
             break
 
         client.send(reply.encode())
+        
 finally:
     client.close()
     print(Fore.RED + f"{address} is out")
